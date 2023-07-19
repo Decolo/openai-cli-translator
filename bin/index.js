c14e735a8a8c1538ff61dc333784aae1e24406bd
caicheng <caicheng@bytedance.com>
2023-07-19 18:21:21 +0800

    fix: change to commander

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
-const args = require("yargs").argv;
-debugger;
-require("../lib/cjs/index").run({
-  language: args.language,
-  text: args["_"][0],
-  token: args.apiKey,
-});
+const { program } = require('commander');
+
+program
+  .option('-l, --language <char>')
+  .option('-k, --key <char>');
+
+program.parse();
+
+const options = program.opts();
+
+require('../lib/cjs/index.js').run({
+  language: options.language,
+  key: options.key,
+  text: program.args[0]
+})
